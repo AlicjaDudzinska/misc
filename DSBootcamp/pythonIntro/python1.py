428c2aab2935e9b615a9016528050f0202f1ea08
AlicjaDudzinska <dudzinska.ala@gmail.com>
2022-10-27 22:22:35 +0200

    "next steps"

--- DSBootcamp/pythonIntro/python1.py
+++ DSBootcamp/pythonIntro/python1.py
@@ -9,3 +9,9 @@ YO
 '''
 
 print(long_string)
+
+from datetime import date
+birth_year = int(input("What year were you born? "))
+year_now = date.today().year
+age = year_now - birth_year
+print(f"You are {age} years old")
--- DSBootcamp/pythonIntro/python2.py
+++ DSBootcamp/pythonIntro/python2.py
@@ -19,3 +19,21 @@ day = 100
 print(f'Hi {name}. It\'s day {day}!')
 print('Hi {0}. It\'s day {1}'.format(name, day))
 print('Hi {1}. It\'s day {0}'.format(name, day))
+
+
+selfish = "scooby doo pa pa"
+print(selfish[6])
+print(selfish[2:])
+print(selfish[2::2])
+print(selfish[-5])
+print(selfish[::-1])
+
+quote = "to be or not to be"
+print(quote.upper())
+print(quote.capitalize())
+print(quote.find("not"))
+print(quote.replace("not", "yeah"))
+
+is_cool = True
+print(type(is_cool))
+print(bool(0))
